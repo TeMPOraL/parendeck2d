86a9860d75213ec2722690f42fbc6c1671676b4e
Jacek TeMPOraL Złydach <temporal.pl@gmail.com>
2017-02-21 05:07:08 +0100

    Switched internal timer to high performance counter.
    It should now have sub-millisecond resolution on most (interesting)
    platforms.
    
    + some updates to TODO file.

--- core/time.lisp
+++ core/time.lisp
@@ -3,21 +3,25 @@
 
 ;;; Current time getters. Just to standardize on an API.
 
-(declaim (inline get-current-milliseconds))
-(defun get-current-milliseconds ()
-  "Gets a high-resolution time as an integer in milliseconds since some arbitrary point in time.
-To be used to compute differences."
-  ;; NOTE we don't worry about sdl2:get-ticks wrapping around, since the documentation says
-  ;; it returns an uint32 counting milliseconds from library initialization, which gives us
-  ;; around 72 days before wrap-around.
-  (sdl2:get-ticks))
-
 (declaim (inline get-current-seconds))
 (defun get-current-seconds ()
+  (declare (optimize (speed 3)))
   "Get a high-resolution time as a float in seconds since some arbitrary point in time.
 To be used to compute differences."
-  (float (/ (get-current-milliseconds)
-            1000)))
+  (coerce (/ (sdl2:get-performance-counter)
+             (sdl2:get-performance-frequency))
+          'double-float))
+
+(declaim (inline get-current-milliseconds))
+(defun get-current-milliseconds ()
+  (declare (optimize (speed 3)))
+  "Gets a high-resolution time as a float in milliseconds since some arbitrary point in time.
+To be used to compute differences.
+
+Note that the result is a float - the timer may have higher resolution than 1ms."
+  (coerce (* 1000 (/ (sdl2:get-performance-counter)
+                     (sdl2:get-performance-frequency)))
+          'double-float))
 
 
 ;;; Additional time-related utils will go here.
